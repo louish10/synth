776e236573246acb71f227a00f3fd5989a529f45
Louis Headley <louish10@hotmail.co.uk>
2023-11-06 11:13:22 +0000

    Improve logic for ADSR controls

--- src/components/adsr-controller/AdsrController.tsx
+++ src/components/adsr-controller/AdsrController.tsx
@@ -6,8 +6,9 @@ import range from 'lodash'
 
 export default function AdsrController() {
     const [attack, setAttack] = useState(50)
+    const [attackHeight, setAttackHeight] = useState(50)
     const [decay, setDecay] = useState(50)
-    const [sustain, setSustain] = useState(50)
+    const [sustain, setSustain] = useState(25)
     const [release, setRelease] = useState(50)
     let ref = useRef<HTMLCanvasElement>(null);
 
@@ -15,27 +16,35 @@ export default function AdsrController() {
         let requestId:number;
         let canvas = ref.current;
         let context:CanvasRenderingContext2D | null;
+        const scale = 1000;
         if (canvas){
             context = canvas.getContext('2d');
+            canvas.width = scale;
+            canvas.height = scale;
         }
         const render = () => {
             if (context && canvas) {
                 context.clearRect(0, 0, canvas.width, canvas.height)
                 context.beginPath();
                 context.moveTo(0, canvas.height);
-                context.lineTo(attack, 10);
+                context.lineWidth = 10;
+                const scaledAttack = attack / 400 * scale;
+                const scaledAttackHeight = attackHeight / 100 * scale;
+                const scaledDecay = decay / 400 * scale;
+                const scaledSustain = sustain / 100 * scale;
+                const scaledSustainLength = scale/4
+                const scaledRelease = release / 400 * scale;
+
+                context.lineTo(scaledAttack, canvas.height - scaledAttackHeight);
                 context.stroke();
 
-                // const xDecay = [attack, attack + 2, attack + 4]
-                // const yDecay = xDecay.map((x) => attack*Math.exp(x - attack))
-
-                context.lineTo(attack + decay, canvas.height - sustain)
+                context.lineTo(scaledAttack + scaledDecay, canvas.height - scaledSustain)
                 context.stroke();
 
-                context.lineTo(attack + decay + 50, canvas.height - sustain)
+                context.lineTo(scaledAttack + scaledDecay + scaledSustainLength, canvas.height - scaledSustain)
                 context.stroke();
 
-                context.lineTo(attack+decay+50+release, canvas.height)
+                context.lineTo(scaledAttack+scaledDecay+scaledSustainLength+scaledRelease, canvas.height + 10)
                 context.stroke();
             }
             requestId = requestAnimationFrame(render);
@@ -45,10 +54,18 @@ export default function AdsrController() {
             cancelAnimationFrame(requestId)
         }
     })
+
+    function updateAttackHeight(value:number) {
+        setAttackHeight(value)
+        if (value < sustain) {
+            setSustain(value)
+        }
+
+    }
     return <div>
         <canvas ref={ref} className="adsr-visualiser"></canvas>
         <div>
-            Attack
+            Attack time
             <Slider
                 value={attack}
                 onChange={(e, v) => { if (typeof(v) === 'number') {
@@ -59,7 +76,18 @@ export default function AdsrController() {
             />
         </div>
         <div>
-            Decay
+            Attack height
+            <Slider
+                value={attackHeight}
+                onChange={(e, v) => { if (typeof(v) === 'number') {
+                    updateAttackHeight(v) }}}
+                min={25}
+                max={100}
+                valueLabelDisplay='auto'
+            />
+        </div>
+        <div>
+            Decay time
             <Slider
                 value={decay}
                 onChange={(e, v) => { if (typeof(v) === 'number') {
@@ -70,18 +98,18 @@ export default function AdsrController() {
             />
         </div>
         <div>
-            Sustain
+            Sustain height
             <Slider
                 value={sustain}
                 onChange={(e, v) => { if (typeof(v) === 'number') {
                     setSustain(v) }}}
                 min={10}
-                max={100}
+                max={attackHeight}
                 valueLabelDisplay='auto'
             />
         </div>
         <div>
-            Release
+            Release time
             <Slider
                 value={release}
                 onChange={(e, v) => { if (typeof(v) === 'number') {
